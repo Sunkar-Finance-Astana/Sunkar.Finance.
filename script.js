2b2c2ee61d9ce39bff9bf9dce8ed2bbc130565a3
Sunkar-Finance-Astana <sunkarfinance@yandex.kz>
2025-11-23 21:36:35 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function sendAgent(){
-  const f=document.getElementById('agentForm');
-  const name=f.name.value;
-  const phone=f.phone.value;
-  const city=f.city.value;
-  const msg=`Хочу стать агентом. Имя: ${name}, Тел: ${phone}, Город: ${city}`;
-  window.location=`https://wa.me/77052606667?text=${encodeURIComponent(msg)}`;
-}
